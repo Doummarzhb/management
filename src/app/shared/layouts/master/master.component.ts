172771b5553d7dcecbc5af15ebf75d8453e0d888
Doummar M.alzahabi <98951369+Doummarzhb@users.noreply.github.com>
2024-02-23 11:09:03 +0200

    code

---
+++ src/app/shared/layouts/master/master.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {CommonModule } from '@angular/common';
+import {RouterModule } from '@angular/router';
+import {HeaderComponent} from '../header/header.component';
+
+
+@Component({
+  selector: 'app-master',
+  standalone: true,
+  imports: [RouterModule,CommonModule,HeaderComponent],
+  templateUrl: './master.component.html',
+  styleUrl: './master.component.scss'
+})
+export class MasterComponent {
+
+}
